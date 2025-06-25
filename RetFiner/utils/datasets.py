cfea5ec3a8b4924e5701ca37b2d20e31a37161e6
Ronnie <ronnief1@vt.edu>
2025-06-25 15:59:15 +0200

    Add files via upload

---
+++ RetFiner/utils/datasets.py
@@ -0,0 +1,63 @@
+import torchvision.transforms.functional as TF
+from torchvision import datasets, transforms
+
+
+from .data_constants import (IMAGENET_DEFAULT_MEAN,
+                             IMAGENET_DEFAULT_STD, IMAGENET_INCEPTION_MEAN,
+                             IMAGENET_INCEPTION_STD)
+from .dataset_folder import ImageFolder
+
+
+
+def denormalize(img, mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD):
+    return TF.normalize(
+        img.clone(),
+        mean= [-m/s for m, s in zip(mean, std)],
+        std= [1/s for s in std]
+    )
+
+
+def build_dataset(is_train, args):
+    transform = build_transform(is_train, args)
+
+    print("Transform = ")
+    if isinstance(transform, tuple):
+        for trans in transform:
+            print(" - - - - - - - - - - ")
+            for t in trans.transforms:
+                print(t)
+    else:
+        for t in transform.transforms:
+            print(t)
+    print("---------------------------")
+
+    if args.data_set == 'CIFAR':
+        dataset = datasets.CIFAR100(args.data_path, train=is_train, transform=transform)
+        nb_classes = 100
+    elif args.data_set == 'IMNET':
+        # root = os.path.join(args.data_path, 'train' if is_train else 'val')
+        root = args.data_path if is_train else args.eval_data_path
+        dataset = datasets.ImageFolder(root, transform=transform)
+        nb_classes = 1000
+    elif args.data_set == "image_folder":
+        root = args.data_path if is_train else args.eval_data_path
+        dataset = ImageFolder(root, transform=transform)
+        nb_classes = args.nb_classes
+        assert len(dataset.class_to_idx) == nb_classes
+    else:
+        raise NotImplementedError()
+    assert nb_classes == args.nb_classes
+    print("Number of the class = %d" % args.nb_classes)
+
+    return dataset, nb_classes
+
+
+def build_transform(is_train, args):
+    resize_im = args.input_size > 32
+    imagenet_default_mean_and_std = args.imagenet_default_mean_and_std
+    mean = IMAGENET_INCEPTION_MEAN if not imagenet_default_mean_and_std else IMAGENET_DEFAULT_MEAN
+    std = IMAGENET_INCEPTION_STD if not imagenet_default_mean_and_std else IMAGENET_DEFAULT_STD
+
+    return transforms.Compose([
+        transforms.ToTensor(),
+    ])
